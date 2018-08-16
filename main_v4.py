aa19219c604628c50142a58b5b94f017d9e3192d
Haigang Liu <haigang@email.sc.edu>
2018-08-15 20:24:13 -0400

    passed major test

--- main_v4.py
+++ main_v4.py
@@ -9,27 +9,31 @@ from data_constructors import DataConstructor
 from helper_functions import customize_cross_entropy_function
 
 from customized_models import ModelGenerator
-from trainingEngine_v2 import ModelTrainingAndTesting
+from training_engine import ModelTrainingAndTesting
 from label_generator import label_generator
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 torch.backends.cudnn.benchmark = True
-
 train_file, val_file, test_file = label_generator(INFO_DIR, 'Image Index','Finding Labels')
+
 train_dataset = DataConstructor(DATA_DIR, ground_truth=train_file, image_size = IMAGE_SIZE)
 val_dataset = DataConstructor(DATA_DIR, ground_truth=val_file, image_size = IMAGE_SIZE)
 test_dataset = DataConstructor(DATA_DIR, ground_truth=test_file, image_size = IMAGE_SIZE)
 
-DATA_AUGMENTATION = False
+
 if DATA_AUGMENTATION:
-    train_dataset = DataAugmentor(train_file, which_class=0, sample_size=50).concat_original_dataset(train_dataset)
+    train_dataset = DataAugmentor(ground_truth_file=train_file,
+                                 # data_folder=DATA_DIR,
+                                 # new_data_folder=LOG_DIR, #anywhere is ok
+                                 which_class= AUGMENT_CLASS,
+                                 sample_size=50,
+                                 ).concat_original_dataset(train_dataset)
 else:
     logging.info('Original dataset has been used. No data augmentation')
 
-sampler_train = Sampler(train_dataset, 'both', sample_size=75000).generate_sampler()
+sampler_train = Sampler(train_dataset, SAMPLER_TYPE, sample_size=SAMPLE_SIZE).generate_sampler()
 sampler_val = Sampler(test_dataset, 'other', sample_size=None).generate_sampler()
 sampler_test = Sampler(val_dataset, 'other', sample_size=None).generate_sampler()
-
 
 train_dataloader = DataLoader(dataset = train_dataset,
                               batch_size = BATCH_SIZE,
@@ -56,15 +60,15 @@ test_dataloader = DataLoader(dataset = test_dataset,
                             drop_last = True)
 
 model_ft = ModelGenerator(MODEL_NAME,
-                    pretrain = True,
-                    freeze_all_layers_before_last = False).\
+                    pretrain = PRETRAIN,
+                    freeze_all_layers_before_last = TRANSFER_FROM_IMAGE_NET).\
             network_modifier(NUM_CLASS).\
             change_initialization(ALTERNATIVE_INITIALIZATION).\
             get_model().\
             to(device)
 
 if BINARY:
-    cross_entropy_loss = customize_cross_entropy_function(train_dataset, 'auto', verbose = False)
+    cross_entropy_loss = customize_cross_entropy_function(train_dataset, WEIGHTS_TENSOR)
 else:
     cross_entropy_loss = None #use default
 
